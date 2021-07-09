7b5d948d63378466fbe0cfcc7831114b16aff83c
webseb00 <lordow91@gmail.com>
2021-07-09 20:53:30 +0200

    Update game script

--- src/Controller.js
+++ src/Controller.js
@@ -12,22 +12,59 @@ class Controller {
     const regex = /[a-zA-Z]+/g;
     const { gameState } = this.model;
     const { setMessage, clearMessage } = this.view;
-    console.log(inputValue);
+    
+    if(!regex.test(inputValue)) {
+      setMessage('warning', 'Please enter your name!');
+      setTimeout(() => clearMessage(), 4000);
+      return false;
+    }
 
     gameState.playerName = inputValue;
     this.setCurrentPage();
-
   }
 
   setCurrentPage = () => {
     const { gameState } = this.model;
-    const { setDashboardPage, endGameButton } = this.view;
+    const { setDashboardPage, endGameButton, createGameCards } = this.view;
 
     if(gameState.currentPage === 'welcome') {
       gameState.currentPage = 'dashboard';
       setDashboardPage();
+      this.handleGamePlay();
       endGameButton.addEventListener('click', this.cancelGame);
     } 
+  }
+
+  handleGamePlay = () => {
+    const { gameplayLevel, playerPoints } = this.model.gameState;
+    const { gamePlayTimer, addCardColor, gameBoard, createGameCards } = this.view;
+
+    let numberOfCards;
+    if(gameplayLevel === 'easy-level') { numberOfCards = 16; }
+    if(gameplayLevel === 'medium-level') { numberOfCards = 25; }
+    if(gameplayLevel === 'hard-level') { numberOfCards = 36; }
+
+    createGameCards(gameplayLevel, numberOfCards);
+
+    let clickedCardNumber = null;
+    let randomNumber = null;
+
+    gameBoard.addEventListener('click', e => {
+      if(e.target.classList.contains('game__board-card')) {
+        clickedCardNumber = e.target.id.split('-')[1];
+        
+        if(clickedCardNumber === randomNumber.toString()) {
+          console.log(clickedCardNumber);
+          ++this.model.gameState.playerPoints;
+          console.log(`Player points: ${this.model.gameState.playerPoints}`);
+        }
+      }
+    });
+
+    setInterval(() => {
+      randomNumber = Math.floor(Math.random() * numberOfCards) + 1;
+      addCardColor(randomNumber);
+    }, 2000);
   }
 
   cancelGame = () => {
@@ -39,7 +76,12 @@ class Controller {
     setWelcomePage();
     endGameButton.removeEventListener('click', this.cancelGame);
     startGameButton.removeEventListener('click', this.handleStartGame);
-    difficultyLevelButtons.forEach(button => button.removeEventListener('click', e => this.setGamplayLevel(e)));
+    difficultyLevelButtons.forEach(button => {
+      button.removeEventListener('click', e => this.setGamplayLevel(e));
+      button.checked = false;
+    });
+    difficultyLevelButtons[0].checked = true;
+
     this.initFunctions();
   }
 
@@ -49,9 +91,11 @@ class Controller {
   }
 
   initFunctions = () => {
-    const { startGameButton, difficultyLevelButtons } = this.view;
+    const { gameplayLevel } = this.model.gameState;
+    const { startGameButton, difficultyLevelButtons, clearGameBoard } = this.view;
     difficultyLevelButtons.forEach(button => button.addEventListener('click', e => this.setGameplayLevel(e)));
     startGameButton.addEventListener('click', this.handleStartGame);
+    clearGameBoard(gameplayLevel);
   }
 }
 
--- src/View.js
+++ src/View.js
@@ -8,6 +8,8 @@ class View {
     this.startGameButton = document.querySelector('#start_game_button');
     this.infoBox = document.querySelector('#info-box');
     this.endGameButton = document.querySelector('#end-game-button');
+    this.gameBoard = document.querySelector('.game__board');
+    this.timer = document.querySelector('.timer');
   };
 
   setDashboardPage = () => {
@@ -20,6 +22,32 @@ class View {
     this.welcomePage.style.display = 'block';
   }
 
+  createGameCards = (gameLevel, numberOfCards) => {
+    let cards = ``;
+
+    for(let i = 1; i <= numberOfCards; i++) {
+      cards += `
+        <div id="card-${i}" class="game__board-card"></div>
+      `;
+    }
+
+    this.gameBoard.classList.add(`game__board--${gameLevel}`);
+    this.gameBoard.insertAdjacentHTML('afterbegin', cards);
+  }
+
+  addCardColor = (randomNumber) => {
+    const findCardByNumber = document.querySelector(`#card-${randomNumber}`);
+    findCardByNumber.classList.add('bg-primary');
+  }
+
+  removeCardColor = () => {
+
+  }
+
+  gamePlayTimer = () => {
+
+  }
+
   setMessage = (type, text) => {
     const alert = `
       <div class="alert alert-${type}" role="alert">
@@ -27,6 +55,11 @@ class View {
       </div>
     `;
     this.infoBox.insertAdjacentHTML('afterbegin', alert);
+  }
+
+  clearGameBoard = (gameLevel) => {
+    this.gameBoard.innerHTML = '';
+    this.gameBoard.classList.remove(`game__board--${gameLevel}`);
   }
 
   clearMessage = () => {
